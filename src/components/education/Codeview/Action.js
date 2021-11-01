42d8c1c622fa7f8a885fd86422f69079458a383c
Luca Vitali <35101914+Iucapad@users.noreply.github.com>
2021-11-01 20:15:31 +0100

    progress

--- src/components/education/Codeview/Action.js
+++ src/components/education/Codeview/Action.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+const icons = {
+    "pivot": require("./assets/cv_pivot.svg").default,
+    "go": require("./assets/cv_go.svg").default,
+}
+
 const Action = ({ action }) => {
     return (
         <div>
-            {action.command} {action.option} {action.length}
+            <img draggable="false" width="50" height="50" src={ icons[action.command] } alt=""/>
+            {action.option} {action.length}
         </div>
     );
 }
